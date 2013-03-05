a2ed7df02c46c9847631e8d86392259e2223b032
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-05 21:49:54 +0000

    Add integer add function

--- src/zadd.c
+++ src/zadd.c
@@ -6,5 +6,41 @@
 
 void bignum_add(BigNum *res, BigNum *left, BigNum *right)
 {
-     
+     BigNum *t;
+
+     /* ensure that left->length >= right->length */
+     if (left->length < right->length) {
+          t = left;
+          left = right;
+          right = t;
+     }
+
+     if (res->max_length <= left->length) {
+          bignum_realloc(res, left->length+1);
+     }
+
+     if (left->neg ^ right->neg) {
+          /* different signs */
+          if (left->length != right->length) {
+               /* so left->length > right->length */
+               bignum_nsub(res, left, right);
+               res->neg = left->neg;
+          }
+          /* left->length == right->length */
+          else if (bignum_ncmp(*left, *right) < 0) {
+               /* so left < right*/
+               bignum_nsub(res, right, left);
+               res->neg = right->neg;
+          }
+          else {
+               /* so left >= right */
+               bignum_nsub(res, left, right);
+               res->neg = left->neg;
+          }
+     }
+     else {
+          /* same signs */
+          bignum_nadd(res, left, right);
+          res->neg = left->neg;
+     }
 }
