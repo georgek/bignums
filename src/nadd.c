d03df09a0134cb1c4570fb65013c9f0f55e6b2c9
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-05 19:58:18 +0000

    Fix nadd, taking into account left->length >= right->length

--- src/nadd.c
+++ src/nadd.c
@@ -8,17 +8,18 @@ void bignum_nadd(BigNum *res, BigNum *left, BigNum *right)
      unsigned t, k;
      int i;
 
-     res->length = left->length > right->length ? left->length : right->length;
      k = 0;
-     for (i = 0; i < res->length; ++i) {
+     for (i = 0; i < right->length; ++i) {
           t = left->digits[i] + right->digits[i] + k;
           res->digits[i] = t;   /* ie. t & 0xFFFF */
           k = t >> WORD_LENGTH;
      }
-     if (k) {
-          res->digits[i] = k;
-          res->length++;
+     for (; i < left->length; ++i) {
+          res->digits[i] = left->digits[i] + k;
+          k = (0 == res->digits[i]); /* overflow? */
      }
+     res->digits[i] = k;
+     res->length = left->length + k;
 }
 
 void bignum_nadd2(BigNum *res, BigNum *left, SHORT_INT_T right)
@@ -33,11 +34,6 @@ void bignum_nadd2(BigNum *res, BigNum *left, SHORT_INT_T right)
           res->digits[i] = left->digits[i] + k;
           k = (0 == res->digits[i]); /* overflow? */
      }
-     if (k) {
-          res->digits[i] = k;
-          res->length = left->length + 1;
-     }
-     else {
-          res->length = left->length;
-     }
+     res->digits[i] = k;
+     res->length = left->length + k;
 }
