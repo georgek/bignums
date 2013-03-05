da1fea7a01bf5779e68836066e488799f67b487c
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-05 22:32:38 +0000

    Move zadd and zsub into common header file like GMP

---
+++ src/addorsub.h
@@ -0,0 +1,61 @@
+/* Integer addition and subtraction are almost identical functions. */
+
+/* Ok, I basically stole this from GMP... */
+
+#include "bignum.h"
+#include "natural.h"
+#include "integer.h"
+
+#ifdef BN_OP_zadd
+#define FUNCTION  bignum_add
+#define VARIATION
+#endif
+#ifdef BN_OP_zsub
+#define FUNCTION  bignum_sub
+#define VARIATION !
+#endif
+
+#ifndef FUNCTION
+Error, need OPERATION_add or OPERATION_sub
+#endif
+
+void FUNCTION (BigNum *res, BigNum *left, BigNum *right)
+{
+     BigNum *t;
+
+     /* ensure that left->length >= right->length */
+     if (left->length < right->length) {
+          t = left;
+          left = right;
+          right = t;
+     }
+
+     if (res->max_length <= left->length) {
+          bignum_realloc(res, left->length+1);
+     }
+
+     if (left->neg ^ VARIATION right->neg) {
+          /* different signs */
+          if (left->length != right->length) {
+               /* so left->length > right->length */
+               bignum_nsub(res, left, right);
+               res->neg = left->neg;
+          }
+          /* left->length == right->length */
+          else if (bignum_ncmp(*left, *right) < 0) {
+               /* so left < right*/
+               bignum_nsub(res, right, left);
+               res->neg = VARIATION right->neg;
+          }
+          else {
+               /* so left >= right */
+               bignum_nsub(res, left, right);
+               res->neg = left->neg;
+          }
+     }
+     else {
+          /* same signs */
+          bignum_nadd(res, left, right);
+          res->neg = left->neg;
+     }
+}
--- src/zsub.c
+++ src/zsub.c
@@ -5,48 +5,10 @@
 #include "natural.h"
 #include "integer.h"
 
+#define BN_OP_zsub
+#include "addorsub.h"
+
 void bignum_negate(BigNum *p)
 {
      p->neg = !p->neg;
-}
-
-void bignum_sub(BigNum *res, BigNum *left, BigNum *right)
-{
-     BigNum *t;
-
-     /* ensure that left->length >= right->length */
-     if (left->length < right->length) {
-          t = left;
-          left = right;
-          right = t;
-     }
-
-     if (res->max_length <= left->length) {
-          bignum_realloc(res, left->length+1);
-     }
-
-     if (left->neg ^ !right->neg) {
-          /* different signs */
-          if (left->length != right->length) {
-               /* so left->length > right->length */
-               bignum_nsub(res, left, right);
-               res->neg = left->neg;
-          }
-          /* left->length == right->length */
-          else if (bignum_ncmp(*left, *right) < 0) {
-               /* so left < right*/
-               bignum_nsub(res, right, left);
-               res->neg = !right->neg;
-          }
-          else {
-               /* so left >= right */
-               bignum_nsub(res, left, right);
-               res->neg = left->neg;
-          }
-     }
-     else {
-          /* same signs */
-          bignum_nsub(res, left, right);
-          res->neg = left->neg;
-     }
 }
