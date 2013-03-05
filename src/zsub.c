7044e344fa1f956b6a6c9ec98c72b5bc651d9202
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-05 22:21:16 +0000

    Add integer subtraction

--- src/zsub.c
+++ src/zsub.c
@@ -1,4 +1,5 @@
-/* Integer subtraction. */
+/* Integer subtraction.
+ * TODO: merge this code with zadd since its virtually identical. */
 
 #include "bignum.h"
 #include "natural.h"
@@ -11,5 +12,41 @@ void bignum_negate(BigNum *p)
 
 void bignum_sub(BigNum *res, BigNum *left, BigNum *right)
 {
-     
+     BigNum *t;
+
+     /* ensure that left->length >= right->length */
+     if (left->length < right->length) {
+          t = left;
+          left = right;
+          right = t;
+     }
+
+     if (res->max_length <= left->length) {
+          bignum_realloc(res, left->length+1);
+     }
+
+     if (left->neg ^ !right->neg) {
+          /* different signs */
+          if (left->length != right->length) {
+               /* so left->length > right->length */
+               bignum_nsub(res, left, right);
+               res->neg = left->neg;
+          }
+          /* left->length == right->length */
+          else if (bignum_ncmp(*left, *right) < 0) {
+               /* so left < right*/
+               bignum_nsub(res, right, left);
+               res->neg = !right->neg;
+          }
+          else {
+               /* so left >= right */
+               bignum_nsub(res, left, right);
+               res->neg = left->neg;
+          }
+     }
+     else {
+          /* same signs */
+          bignum_nsub(res, left, right);
+          res->neg = left->neg;
+     }
 }
