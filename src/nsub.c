211ca57d78160beb4bc6b71e93213e671901f945
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-05 19:58:44 +0000

    Add nsub functions

--- src/nsub.c
+++ src/nsub.c
@@ -6,7 +6,28 @@
 
 void bignum_nsub(BigNum *res, BigNum *left, BigNum *right)
 {
+     unsigned t, k, res_length;
+     int i;
 
+     k = 0;
+     res_length = 0;
+     for (i = 0; i < right->length; ++i) {
+          t = left->digits[i] - right->digits[i] - k;
+          res->digits[i] = t;
+          k = (~(~0u>>1) & t) != 0;
+          if (res->digits[i] != 0) {
+               res_length = i + 1;
+          }
+     }
+     for (; i < left->length; ++i) {
+          t = left->digits[i] - k;
+          res->digits[i] = t;
+          k = (~(~0u>>1) & t) != 0;
+          if (res->digits[i] != 0) {
+               res_length = i + 1;
+          }
+     }
+     res->length = res_length;
 }
 
 void bignum_nsub2(BigNum *res, BigNum *left, SHORT_INT_T right)
@@ -15,11 +36,11 @@ void bignum_nsub2(BigNum *res, BigNum *left, SHORT_INT_T right)
      int i;
 
      i=0;
+     k = (left->digits[i] < right); /* will overflow happen? */
      res->digits[i] = left->digits[i] - right;
-     k = (res->digits[i] > right);    /* did overflow happen? */
      for (i=1; i < left->length; ++i) {
           res->digits[i] = left->digits[i] - k;
-          k = (~0 == res->digits[i]); /* overflow? */
+          k = (RADIX-1 == res->digits[i]);
      }
      if (!res->digits[i-1]) {
           res->length = left->length - 1;
@@ -28,3 +49,4 @@ void bignum_nsub2(BigNum *res, BigNum *left, SHORT_INT_T right)
           res->length = left->length;
      }
 }
+
