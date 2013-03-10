4e71634cd25a4eae6ff75788d18db6b6d6862c2b
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-10 22:36:35 +0000

    Add long division

--- src/ndiv.c
+++ src/ndiv.c
@@ -1,12 +1,111 @@
 /* natural number division */
 
 #include <stdlib.h>
+#include <stdio.h>
 
+#include "bignum.h"
 #include "natural.h"
+#include "memory.h"
 
 void bignum_ndiv(BigNum *q, BigNum *r, BigNum *left, BigNum *right)
 {
+     BigNum *oq = NULL, nq = *q;
+     BigNum leftn, rightn;      /* normalised forms */
+     SHORT_INT_T qhat, rhat;    /* guesses */
+     SHORT_INT_T pu, pl;
+     unsigned s, m, n;
+     int i,j;
+     SHORT_INT_T t,t2,k,k2;
+
+     m = left->length;
+     n = right->length;
+
+     nq.length = m - n + 1;
      
+     if (q == left || q == right) {
+          /* need to setup new bignum so we don't clobber left or right */
+          oq = q;
+          nq.max_length = nq.length;
+          nq.digits = malloc(sizeof(SHORT_INT_T)*nq.max_length);
+     }
+
+     /* Normalise by shifting right left just enough so that its high-order
+      * bit is on, and shift left by the same amount. Make sure to increase
+      * the length of left by 1. */
+
+     s = nlz(right->digits[n - 1]);
+     leftn = bignum_copy(left);
+     rightn = bignum_copy(right);
+     bignum_lshift(&leftn, s);
+     bignum_lshift(&rightn, s);
+     if (leftn.length == m) {
+          bignum_realloc(&leftn, m+1);
+     }
+
+     for (j = nq.length - 1; j >= 0; --j) {
+          /* compute estimate of qhat of q[j] */
+          dig_div(&qhat, &rhat,
+                  leftn.digits[j+n], leftn.digits[j+n-1],
+                  rightn.digits[n-1]);
+          if (qhat == ~0 && rhat == ~0) {
+               /* qhat overflowed, set qhat <- b-1 (already done) and rhat <-
+                * u_(j+n-1) */
+               rhat = rightn.digits[n-1];
+          }
+     again:
+          /* At this point, qhat < b; now we do the test(s) on v_(n-2). */
+          dig_mul(&pu, &pl, qhat, rightn.digits[n-2]);
+          if (pu > rhat || (pu == rhat && pl > leftn.digits[j+n-2])) {
+               --qhat;
+               rhat = rhat + rightn.digits[n-1];
+               if (rhat >= rightn.digits[n-1]) {
+                    /* didn't overflow */
+                    goto again;
+               }
+          }
+
+          /* multiply and subtract */
+          k=0,k2=0;
+          for (i =0; i < n; ++i) {
+               t = k;
+               /* (k, t)_b <- v_(i) * qhat */
+               dig_mulandadd(&k, &t, rightn.digits[i], qhat);
+               t2 = leftn.digits[i] - t - k2;
+               k2 = (k2 ? t2 >= leftn.digits[i] : t2 > leftn.digits[i]);
+               leftn.digits[i] = t2;
+          }
+          t2 = leftn.digits[i] - k - k2;
+          k2 = (k2 ? t2 >= leftn.digits[i] : t2 > leftn.digits[i]);
+          leftn.digits[i] = t2;
+
+          nq.digits[j] = qhat;
+          /* if result of mul and sub was negative we need to add back */
+          if (k2) {
+               nq.digits[j] = nq.digits[j] - 1;
+               k = 0;
+               for (i = 0; i < n; ++i) {
+                    t = leftn.digits[i+j] + rightn.digits[i] + k;
+                    k = (k ? t <= leftn.digits[i] : t < leftn.digits[i]);
+                    leftn.digits[i+j] = t;
+               }
+               leftn.digits[i+j] = leftn.digits[i+j] + k;
+          }
+     }
+
+     /* work out real size of nq */
+     if (nq.digits[nq.length - 1] == 0) {
+          nq.length--;
+     }
+
+     /* unnormalise the remainder */
+     bignum_rshift(&leftn, s);
+     if (r) {
+          bignum_free(r);
+          *r = leftn;
+     }
+
+     *q = nq;
+     bignum_free(oq);
 }
 
 void bignum_ndiv2(BigNum *q, SHORT_INT_T *r, BigNum *left, SHORT_INT_T right)
@@ -31,19 +130,6 @@ void bignum_ndiv2(BigNum *q, SHORT_INT_T *r, BigNum *left, SHORT_INT_T right)
                   right);
      }
      *r = k;
-}
-
-/* portable (but slow) way to count leading zeros (doesn't matter as it's used
- * only once in div below) */
-int nlz(unsigned x)
-{
-     unsigned s = ~(~0u>>1);
-     int i = 0;
-     while (!(x&s) && i < WORD_LENGTH) {
-          ++i;
-          x = x << 1;
-     }
-     return i;
 }
 
 /* From Hacker's Delight */
@@ -79,11 +165,11 @@ void dig_div(SHORT_INT_T *q, SHORT_INT_T *r,
      vn1 = right >> 16;         // Break divisor up into
      vn0 = right & 0xFFFF;      // two 16-bit digits.
 
-     un32 = (uleft << s) | (lleft >> 32 - s) & (-s >> 31);
+     un32 = (uleft << s) | (lleft >> WORD_LENGTH - s) & (-s >> (WORD_LENGTH-1));
      un10 = lleft << s;         // Shift dividend left.
 
-     un1 = un10 >> 16;          // Break right half of
-     un0 = un10 & 0xFFFF;       // dividend into two digits.
+     un1 = un10 >> WORD_LENGTH/2; // Break right half of
+     un0 = un10 & (SRADIX-1);   // dividend into two digits.
 
      q1 = un32/vn1;             // Compute the first
      rhat = un32 - q1*vn1;      // quotient digit, q1.
@@ -106,4 +192,17 @@ again2:
      if (r != NULL)                            // If remainder is wanted,
           *r = (un21*b + un0 - q0*right) >> s; // return it.
      *q = q1*b + q0;
+}
+
+/* portable (but slow) way to count leading zeros (doesn't matter as it's used
+ * only once in div below) */
+int nlz(unsigned x)
+{
+     unsigned s = ~(~0u>>1);
+     int i = 0;
+     while (!(x&s) && i < WORD_LENGTH) {
+          ++i;
+          x = x << 1;
+     }
+     return i;
 }
--- src/zdiv.c
+++ src/zdiv.c
@@ -1,20 +1,100 @@
 /* Integer division. */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "bignum.h"
 #include "natural.h"
 #include "integer.h"
 
+/* either q or r (or both) can actually be NULL */
 void bignum_div(BigNum *q, BigNum *r, BigNum *left, BigNum *right)
 {
-     
+     BigNum nq;
+     unsigned qlen = left->length - right->length + 1;
+     SHORT_INT_T sr;
+     int cmp;
+
+     /* trivial cases */
+     if (left->length < right->length) {
+          /* left < right */
+          printf("triv1\n");
+          if (q) {
+               bignum_set(q, 0);
+          }
+          if (r) {
+               bignum_set(r, right);
+          }
+          return;
+     }
+     if (right->length == left->length) {
+          cmp = bignum_ncmp(*left, *right);
+          
+          if (cmp < 0) {
+               /* left < right */
+               printf("triv2\n");
+               if (q) {
+                    bignum_set_int(q, 0);
+               }
+               if (r) {
+                    bignum_set(r, right);
+               }
+               return;
+          }
+          if (cmp == 0) {
+               /* left == right */
+               printf("triv3\n");
+               if (q) {
+                    bignum_set_int(q, 1);
+                    q->neg = left->neg ^ right->neg;
+               }
+               if (r) {
+                    bignum_set_int(r, 0);
+               }
+               return;
+          }
+     }
+     /* so left > right */
+
+     bignum_alloc_zero(&nq, qlen);
+
+     if (right->length == 1) {
+          bignum_ndiv2(&nq, &sr, left, right->digits[0]);
+          if (q) {
+               q->length = nq.length;
+               q->max_length = nq.max_length;
+               q->digits = nq.digits;
+               nq.digits = NULL;
+          }
+          if (r) {
+               bignum_set_int(r, sr);
+          }
+     }
+     else {
+          bignum_ndiv(&nq, r, left, right);
+          if (q) {
+               q->length = nq.length;
+               q->max_length = nq.max_length;
+               q->digits = nq.digits;
+               nq.digits = NULL;
+          }
+     }
+     if (q) {
+          q->neg = (left->neg ^ right->neg);
+     }
+     if (r) {
+          r->neg = right->neg;
+     }
+
+     bignum_free(&nq);
 }
 
 void bignum_divq(BigNum *q, BigNum *left, BigNum *right)
 {
-     
+     bignum_div(q, NULL, left, right);
 }
 
 void bignum_divr(BigNum *r, BigNum *left, BigNum *right)
 {
-     
+     bignum_div(NULL, r, left, right);
 }
