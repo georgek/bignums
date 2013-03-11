780c853846f3bc0cfce8612f0346127e406846bf
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-11 21:30:49 +0000

    Fix print function

--- src/zio.c
+++ src/zio.c
@@ -6,6 +6,7 @@
 
 #include "bignum.h"
 #include "integer.h"
+#include "memory.h"
 
 void bignum_print(BigNum p)
 {
@@ -19,11 +20,24 @@ void bignum_print(BigNum p)
           return;
      }
 
-     bignum_init(&w);
-     bignum_copy(&w, &p);
+     w = bignum_copy(&p);
 
-     /* array that will be big enough to contain all the GRPOW10 digits */
-     l = (p.length*WORD_LENGTH + 1)/GRPOW10DIGSB2 + 1;
+     /* array that will be big enough to contain all the GRPOW10 digits
+      *
+      * since:
+      *
+      * Let n be the bignum, W_10 = GRPOW10.
+      * GRPOW10DIGSB2 = fl(log_2(W_10 - 1)) + 1 <= fl(log_2(W_10)) + 1.
+      * Number of bits: fl(log_2(n)) + 1 <= p.length * WORD_SIZE
+      * Number of chunks required: fl(log_(W_10)(n)) + 1
+      *
+      * log_(W_10)(n) = (log_2(n))/(log_2(W_10))
+      *              <= (p.length*WORD_SIZE+1)/(log_2(W_10))
+      *              <= (p.length*WORD_SIZE+1)/(GRPOW10DIGSB2 - 1)
+      *
+      * So:
+      * */
+     l = (p.length*WORD_LENGTH + 1)/(GRPOW10DIGSB2 - 1) + 1;
      
      chunks = calloc(l, sizeof(char*));
 
@@ -43,12 +57,17 @@ void bignum_print(BigNum p)
           }
           ++i;
      }
+     
      if (bignum_is_neg(p)) {
           printf("-");
      }
      for (i = i-1; i >= 0; --i) {
           printf("%s", chunks[i]);
+          free(chunks[i]);
      }
      printf("\n");
+
+     bignum_free(&w);
+     free(chunks);
 }
 
