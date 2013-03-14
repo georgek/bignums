4d972fea2f9c9ef5112ac3a803d856428a0c087f
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-14 15:39:32 +0000

    Add important boilerplate stuff to testfun

--- src/arm/test.s
+++ src/arm/test.s
@@ -1,9 +1,10 @@
-        # function to test linking
-        .globl  testfun
-        .type   testfun,@function
+        ;; ARM function definition
+        ;; unsigned testfun(unsigned)
+        .align  2               ; Align to word boundary
+        .arm                    ; This is ARM code
+        .global testfun
+        .type   testfun STT_FUNC
 testfun:
-        add r0, r0, r0
-
+        add     r0, r0, r0
 end_testfun:
-        mov     pc, lr
-        
+        bx      lr
