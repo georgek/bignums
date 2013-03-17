5bf194bd511892e36ffd5528c062b1fe7f735017
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-17 21:53:36 +0000

    Fix x86_64/nadd.s

--- src/x86_64/nadd.s
+++ src/x86_64/nadd.s
@@ -17,31 +17,38 @@ bignum_nadd:
               pushq %rbp            # push old base pointer to stack
               movq  %rsp, %rbp      # copy stack pointer to base pointer
 
-              movq  (%rsi), %rax
-              addq  (%rcx), %rax
-              movq  %rax, (%rdi)
-
-              movq  $1, %r11        # i <- 1
+              xorq  %r11, %r11      # i <- 0 (also clears carry)
+              pushfq
 
 nadd_main:
               cmpq  %r8, %r11       # while i < sright
               jge   nadd_rest
 
               movq  (%rsi,%r11,8), %rax
+              popfq
               adcq  (%rcx,%r11,8), %rax
+              pushfq
               movq  %rax, (%rdi,%r11,8)
               incq  %r11
               jmp   nadd_main
 
 nadd_rest:
               cmpq  %rdx, %r11      # while i <= sleft
-              jg    nadd_end
+              jge   nadd_last
 
               movq  (%rsi,%r11,8), %rax
+              popfq
               adcq  $0, %rax        # just adding on the carry
+              pushfq
               movq  %rax, (%rdi,%r11,8)
               incq  %r11
               jmp   nadd_rest
+
+nadd_last:
+              xorq  %rax, %rax
+              popfq
+              adcq  $0, %rax
+              movq  %rax, (%rdi,%r11,8)
 
 nadd_end:
               movq  %rbp, %rsp      # move stack pointer back
@@ -63,21 +70,31 @@ bignum_nadd2:
               pushq %rbp            # push old base pointer to stack
               movq  %rsp, %rbp      # copy stack pointer to base pointer
 
+              xorq  %r11, %r11
+
               movq  (%rsi), %rax
               addq  %rcx, %rax
+              pushfq
               movq  %rax, (%rdi)
-
-              movq  $1, %r11        # i <- 1
+              incq  %r11
 
 nadd2_main:
-              cmpq  %r8, %r11       # while i < slength
-              jge   nadd2_end
+              cmpq  %rdx, %r11       # while i < sleft
+              jge   nadd2_last
 
               movq  (%rsi,%r11,8), %rax
+              popfq
               adcq  $0, %rax
+              pushfq
               movq  %rax, (%rdi,%r11,8)
               incq  %r11
               jmp   nadd2_main
+
+nadd2_last:
+              xorq  %rax, %rax
+              popfq
+              adcq  $0, %rax
+              movq  %rax, (%rdi,%r11,8)
 
 nadd2_end:
               movq  %rbp, %rsp      # move stack pointer back
