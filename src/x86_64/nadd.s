116367f90e636b6c1ebb4656782102bc520ccd53
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-17 22:30:53 +0000

    Speed up nadd2 by using JRCXZ instruction

--- src/x86_64/nadd.s
+++ src/x86_64/nadd.s
@@ -69,32 +69,35 @@ nadd_end:
 bignum_nadd2:
               pushq %rbp            # push old base pointer to stack
               movq  %rsp, %rbp      # copy stack pointer to base pointer
+              
+              movq  %rcx, %r11      # move right out of %rcx because we need it
+# r11 = right
+              movq  %rdx, %rcx      # now rcx can be decremented sleft times
+                                    # before it is zero
 
-              xorq  %r11, %r11
-
+              xorq  %r8, %r8        # use r8 to index (this also clears carry)
+              
               movq  (%rsi), %rax
-              addq  %rcx, %rax
-              pushfq
+              addq  %r11, %rax
               movq  %rax, (%rdi)
-              incq  %r11
+
+              incq  %r8
+              decq  %rcx
 
 nadd2_main:
-              cmpq  %rdx, %r11       # while i < sleft
-              jge   nadd2_last
+              jrcxz nadd2_last
 
-              movq  (%rsi,%r11,8), %rax
-              popfq
+              movq  (%rsi,%r8,8), %rax
               adcq  $0, %rax
-              pushfq
-              movq  %rax, (%rdi,%r11,8)
-              incq  %r11
+              movq  %rax, (%rdi,%r8,8)
+
+              incq  %r8
+              decq  %rdx
+              
               jmp   nadd2_main
 
 nadd2_last:
-              xorq  %rax, %rax
-              popfq
-              adcq  $0, %rax
-              movq  %rax, (%rdi,%r11,8)
+              adcq  $0, (%rdi,%r8,8)
 
 nadd2_end:
               movq  %rbp, %rsp      # move stack pointer back
