9004edfeea8b51aa16328807d69b2d17b1e62c81
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-17 12:22:34 +0000

    Update nmul functions to new defs

--- src/x86_64/nmul.c
+++ src/x86_64/nmul.c
@@ -4,66 +4,43 @@
 
 #include "../natural.h"
 
-void bignum_nmul(BigNum *res, BigNum *left, BigNum *right)
+void bignum_nmul(SHORT_INT_T *res,
+                 SHORT_INT_T *left, unsigned sleft,
+                 SHORT_INT_T *right, unsigned sright)
 {
-     BigNum *ores = NULL, nres = *res;
      int i,j;
      SHORT_INT_T t,k;
 
-     if (res == left || res == right) {
-          /* need to setup new bignum so we don't clobber left or right */
-          ores = res;
-
-          nres.length = 0;
-          nres.max_length = left->length + right->length;
-          nres.digits = malloc(sizeof(SHORT_INT_T)*nres.max_length);
+     for (i = 0; i < (sleft + sright); ++i) {
+          res[i] = 0;
      }
 
-     for (i = 0; i < (left->length + right->length); ++i) {
-          nres.digits[i] = 0;
-     }
-
-     for (j = 0; j < right->length; ++j) {
+     for (j = 0; j < sright; ++j) {
           k = 0;
-          for (i = 0; i < left->length; ++i) {
+          for (i = 0; i < sleft; ++i) {
                t = k;
-               dig_mulandadd(&k, &t, left->digits[i], right->digits[j]);
-               nres.digits[i+j] += t;
-               k += (nres.digits[i+j] < t); /* might have overflowed */
+               dig_mulandadd(&k, &t, left[i], right[j]);
+               res[i+j] += t;
+               k += (res[i+j] < t); /* might have overflowed */
           }
-          nres.digits[j+left->length] = k;
+          res[j+sleft] = k;
      }
-
-     /* find actual length of nres */
-     for (i = i + j - 1; i >= 0; --i) {
-          if (nres.digits[i] != 0) {
-               nres.length = i + 1;
-               break;
-          }
-     }
-
-     *res = nres;
-     bignum_free(ores);
 }
 
-void bignum_nmul2(BigNum *res, BigNum *left, SHORT_INT_T right)
+void bignum_nmul2(SHORT_INT_T *res,
+                  SHORT_INT_T *left, unsigned sleft,
+                  SHORT_INT_T right)
 {
      SHORT_INT_T t, k;
      int i;
 
      k=0;
-     for (i=0; i < left->length; ++i) {
+     for (i=0; i < sleft; ++i) {
           t = k;
-          dig_mulandadd(&k, &t, left->digits[i], right);
-          res->digits[i] = t;
+          dig_mulandadd(&k, &t, left[i], right);
+          res[i] = t;
      }
-     if (k) {
-          res->digits[i] = k;
-          res->length = left->length + 1;
-     }
-     else {
-          res->length = left->length;
-     }
+     res[i] = k;
 }
 
 /* this does (ures, lres) <- left * right + lres */
