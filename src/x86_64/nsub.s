6592bbe5f9cd61baa43a601db68594c3e54e1598
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-19 17:06:05 +0000

    Add x86-64 nsub and nsub2

---
+++ src/x86_64/nsub.s
@@ -0,0 +1,61 @@
+# natural number subtraction
+# x86_64 version
+
+# void bignum_nsub(SHORT_INT_T *res,
+#                  SHORT_INT_T *left, unsigned sleft,
+#                  SHORT_INT_T *right, unsigned sright);
+
+# rdi = *res
+# rsi = *left
+# rdx = sleft
+# rcx = *right
+# r8 = sright
+
+              .Global bignum_nsub
+              .Type bignum_nsub,@function
+bignum_nsub:
+              pushq %rbp            # push old base pointer to stack
+              movq  %rsp, %rbp      # copy stack pointer to base pointer
+
+              leaq  (%rdi,%r8,8), %rdi # res <- res + sright
+              leaq  (%rsi,%r8,8), %rsi # left <- left + sright
+              leaq  (%rcx,%r8,8), %rcx # right <- right + sright
+              movq  %r8, %r11
+              negq  %r11            # r11 <- -sright
+              clc
+
+nsub_main:
+              movq  (%rsi,%r11,8), %rax
+              sbbq  (%rcx,%r11,8), %rax
+              movq  %rax, (%rdi,%r11,8)
+
+              incq  %r11
+              
+              jnz   nsub_main
+
+nsub_rest_set:
+              pushfq                # push flags
+              movq  %rdx, %rax
+              subq  %r8, %rax       # rax <- sleft - sright
+              jz    nsub_end
+              leaq  (%rdi,%rax,8), %rdi # res <- res + sleft-sright
+              leaq  (%rsi,%rax,8), %rsi # left <- left + sleft-sright
+              movq  %rax, %r11
+              negq  %r11            # r11 <- -(sleft - sright)
+                                    # now r11 can be incremented sleft-sright
+                                    # times before it is zero
+              popfq
+              
+nsub_rest:
+              movq  (%rsi,%r11,8), %rax
+              sbbq  $0, %rax
+              movq  %rax, (%rdi,%r11,8)
+
+              incq  %r11
+              
+              jnz   nsub_rest
+
+nsub_end:
+              movq  %rbp, %rsp      # move stack pointer back
+              popq  %rbp            # restore original base pointer
+              ret                   # pop value from top of stack and go there
---
+++ src/x86_64/nsub2.s
@@ -0,0 +1,45 @@
+# natural number subtraction
+# x86_64 version
+
+# void bignum_nsub2(SHORT_INT_T *res,
+#                   SHORT_INT_T *left, unsigned sleft,
+#                   SHORT_INT_T right)
+
+# rdi = *res
+# rsi = *left
+# rdx = sleft
+# rcx = right
+
+              .Global bignum_nsub2
+              .Type bignum_nsub2,@function
+bignum_nsub2:
+              pushq %rbp            # push old base pointer to stack
+              movq  %rsp, %rbp      # copy stack pointer to base pointer
+
+              leaq  (%rdi,%rdx,8), %rdi # res <- res + sleft
+              leaq  (%rsi,%rdx,8), %rsi # left <- left + sleft
+              movq  %rdx, %r11
+              negq  %r11            # r11 <- -sleft
+                                    # we now increment r11 sleft times before
+                                    # it is zero
+
+              movq  (%rsi,%r11,8), %rax
+              subq  %rcx, %rax
+              movq  %rax, (%rdi,%r11,8)
+
+              incq  %r11
+
+              jz    end_nsub2       # if %r11 is now zero
+nsub2_main:
+              movq  (%rsi,%r11,8), %rax
+              sbbq  $0, %rax
+              movq  %rax, (%rdi,%r11,8)
+
+              incq  %r11
+              
+              jnz   nsub2_main      # if %r11 is not zero
+
+end_nsub2:
+              movq  %rbp, %rsp      # move stack pointer back
+              popq  %rbp            # restore original base pointer
+              ret                   # pop value from top of stack and go there
