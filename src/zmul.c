1f1ef39d30dbd5d56201ddeb8149001e788490a3
George Kettleborough <g.kettleborough@member.fsf.org>
2013-03-22 22:44:08 +0000

    Check for zeros before multipying

--- src/zmul.c
+++ src/zmul.c
@@ -10,6 +10,11 @@ void bignum_mul(BigNum *res, BigNum *left, BigNum *right)
      BigNum nr;
      unsigned reslen = left->length + right->length;
 
+     if(bignum_is_zero(*left) || bignum_is_zero(*right)) {
+          bignum_set_int(res, 0);
+          return;
+     }
+
      bignum_alloc_zero(&nr, reslen);
      nr.neg = left->neg ^ right->neg;
      
